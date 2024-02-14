8867eb578de5e701d8c4a04af175dd4a1b1a5068
oceaniswater <markflyingbear@gmail.com>
2024-02-14 14:35:44 +0000

    create project

---
+++ betstat/betstatApp.swift
@@ -0,0 +1,17 @@
+//
+//  betstatApp.swift
+//  betstat
+//
+//  Created by Mark Golubev on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct betstatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
