19eb1e561f7c6fc5075c99dd04af6df0c5378339
oceaniswater <markflyingbear@gmail.com>
2024-02-15 18:42:16 +0000

    make ProgressBar reusable

--- betstat/BetStatViews/ProgressBar.swift
+++ betstat/BetStatViews/ProgressBar.swift
@@ -5,4 +5,30 @@
 //  Created by Mark Golubev on 15/02/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProgressBar: View {
+    let avgCoefficient: Float
+    let maxCoefficient: Float
+    let progressColor: Color
+    let width: Double
+    
+    private let barOpacity: Double = 0.3
+    private let barHeight: CGFloat = 10
+    private let progressBarOpacity: Double = 0.8
+    private let widthBarPadding: Float = 115
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Rectangle()
+                .frame(width: width - CGFloat(widthBarPadding), height: barHeight)
+                .opacity(barOpacity)
+                .foregroundColor(.gray)
+            
+            Rectangle()
+                .frame(width: CGFloat(avgCoefficient / maxCoefficient) * (width - CGFloat(widthBarPadding)), height: barHeight)
+                .foregroundColor(progressColor)
+                .opacity(progressBarOpacity)
+        }
+    }
+}
